4bf3a9ee87921d93b002cb49141fd8aac1e49984
Petrov Ilya <dnamix1@gmail.com>
2022-01-15 11:57:33 +0300

    is user authenticated route guard

---
+++ src/user/guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { ExpressRequestInterface } from './../../types/expressRequest.interface';
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpStatus,
+  HttpException,
+  Injectable,
+} from '@nestjs/common';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context
+      .switchToHttp()
+      .getRequest<ExpressRequestInterface>();
+    if (!request.user) {
+      throw new HttpException('Not authorized', HttpStatus.UNAUTHORIZED);
+    }
+    return true;
+  }
+}
